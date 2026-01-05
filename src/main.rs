522f1bf8f6aca7987ec4e02ac666c9d17e965c85
Rishabh Kumar <rishabh.6542@gmail.com>
2026-01-05 19:41:12 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -665,7 +665,7 @@ async fn main() -> std::io::Result<()> {
             )
             .service(fs::Files::new("/", "./public").index_file("index.html"))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
